11b66094d5f60993f4a6a598afb9f2f0bd9f883a
wunianguiguolu <gouliguojiashengsiyi@sjtu.edu.cn>
2023-02-23 20:47:19 +0800

    CP2022-23_test.cpp

---
+++ CP2022-23_test.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  SJTU CP2022-23
+//
+//  Created by Chensiyuan on 2023/02/23.
+//
+
+#include <iostream>
+#include <vector>
+
+
+int main(int argc, const char * argv[]) {
+
+	std::cout<<"nihao"<<std::endl;
+	
+	return 0;
+}
